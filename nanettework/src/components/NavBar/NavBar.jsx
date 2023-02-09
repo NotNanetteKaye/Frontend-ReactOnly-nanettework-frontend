a88e1a719d9b6544e21abb5145f1354c50c7dc8b
NotNanetteKaye <nanettekayedolera@gmail.com>
2023-02-08 21:54:11 -0600

    Importé {Link} a NavBar.jsx

--- nanettework/src/components/NavBar/NavBar.jsx
+++ nanettework/src/components/NavBar/NavBar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 
 const NavBar = () => {
@@ -7,6 +8,17 @@ const NavBar = () => {
         <div className='logo'>
             <a href='www.youtube.com'>ND</a>
         </div>
+        <ul>
+            <li>
+                <Link to='/about'>ABOUT</Link>
+            </li>
+            <li>
+                <Link to='/portfolio'>PORTFOLIO</Link>
+            </li>
+            <li>
+                <Link to='/link'>LINK</Link>
+            </li>
+        </ul>
     </div> );
 }
  
