386f1efe53a2c740627deffa9054fe535f3e976a
NotNanetteKaye <nanettekayedolera@gmail.com>
2023-02-13 22:33:27 -0600

    Creé <NavBarLayout/> en NavBar.jsx

--- nanettework/src/components/NavBar/NavBar.jsx
+++ nanettework/src/components/NavBar/NavBar.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const NavBarLayout = styled.div`
+    background: rgb(231 231 231);
+    padding: 4.2mm;
+    position: fixed;
+    width: 100%;
+    display: flex;
+    flex-wrap: wrap;
+    align-items: stretch;
+    justify-content: flex-end;
+    flex-direction: row;
+`
 
 
 const NavBar = () => {
     return ( 
-    <div className='NavBar'>
+    <NavBarLayout>
         <div className='logo'>
             <a href='www.youtube.com'>ND</a>
         </div>
@@ -19,7 +32,7 @@ const NavBar = () => {
                 <Link to='/link'>LINK</Link>
             </li>
         </ul>
-    </div> );
+    </NavBarLayout> );
 }
  
 export default NavBar;
