dc6cec8829b2b10b3d19d8adcfd145c6ee2c26e2
NotNanetteKaye <nanettekayedolera@gmail.com>
2023-02-13 22:36:12 -0600

    Actualicé <NavBarLayout/> para tener info básica

--- nanettework/src/components/NavBar/NavBar.jsx
+++ nanettework/src/components/NavBar/NavBar.jsx
@@ -4,14 +4,10 @@ import styled from 'styled-components';
 
 const NavBarLayout = styled.div`
     background: rgb(231 231 231);
-    padding: 4.2mm;
     position: fixed;
     width: 100%;
     display: flex;
     flex-wrap: wrap;
-    align-items: stretch;
-    justify-content: flex-end;
-    flex-direction: row;
 `
 
 
