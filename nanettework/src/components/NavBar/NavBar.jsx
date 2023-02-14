c05e76d8a066afb2cf1fc73466a52d91347b7f71
NotNanetteKaye <nanettekayedolera@gmail.com>
2023-02-13 23:18:19 -0600

    Arreglé el diseño en NavBar.jsx

--- nanettework/src/components/NavBar/NavBar.jsx
+++ nanettework/src/components/NavBar/NavBar.jsx
@@ -5,39 +5,48 @@ import './NavBar.css';
 
 const NavBarLayout = styled.div`
     background: rgb(231 231 231);
-    text-decoration: none;
     position: fixed;
     width: 100%;
     display: flex;
     flex-wrap: wrap;
     flex-direction: row;
-    justify-content: space-between;
+    justify-content: flex-end;
 `
 const NavBarCategories = styled.ul`
     list-style-type: none;
     font-family: Georgia, 'Times New Roman', Times, serif;
     text-decoration: none;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: flex-end;
+`
+
+const NavBarLogo = styled.a`
+    text-decoration: none;
+    font-size: 40mm;
+    position: fixed;
 `
 
 
 const NavBar = () => {
     return ( 
-    <NavBarLayout>
-        <div className='logo'>
-            <a href='www.youtube.com'>ND</a>
+        <div>
+                <NavBarLogo href='www.youtube.com'>ND
+                </NavBarLogo>
+            <NavBarLayout>
+                <NavBarCategories>
+                    <li>
+                        <Link to='/about'>ABOUT</Link>
+                    </li>
+                    <li>
+                        <Link to='/portfolio'>PORTFOLIO</Link>
+                    </li>
+                    <li>
+                        <Link to='/link'>LINK</Link>
+                    </li>
+                </NavBarCategories>
+            </NavBarLayout> 
         </div>
-        <NavBarCategories>
-            <li>
-                <Link to='/about'>ABOUT</Link>
-            </li>
-            <li>
-                <Link to='/portfolio'>PORTFOLIO</Link>
-            </li>
-            <li>
-                <Link to='/link'>LINK</Link>
-            </li>
-        </NavBarCategories>
-    </NavBarLayout> 
     );
 }
  
