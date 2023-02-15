18a52bfdce7dfc4cf9386e4fc98224afe8c0783c
NotNanetteKaye <nanettekayedolera@gmail.com>
2023-02-15 02:07:49 -0600

    Fixed ratio of grids in NavBar.jsx

--- nanettework/src/components/NavBar/NavBar.jsx
+++ nanettework/src/components/NavBar/NavBar.jsx
@@ -9,7 +9,7 @@ const NavBarLayout = styled.div`
     width: 100%;
     height: 20mm;
     display: grid;
-    grid-template-columns: 20% 70% 10%;
+    grid-template-columns: 20% 60% 20%;
 `
 
 const NavBar = () => {
