c9ffd9a441a6f9f8cbddc29019061778f589c5ae
NotNanetteKaye <nanettekayedolera@gmail.com>
2023-02-14 23:59:16 -0600

    Created <FooterLayout /> styled comp in Footer.js

--- nanettework/src/components/NavBar/Footer/Footer.js
+++ nanettework/src/components/NavBar/Footer/Footer.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const FooterLayout = styled.div`
+    background: rgb(231 231 231);
+    position: fixed;
+    width: 100%;
+    display: grid;
+
+`
 
 const Footer = () => {
     return ( 
-        <div>
+        <FooterLayout>
             <h4>© 2023 nanette kaye dolera. all rights reserved. </h4>
-        </div>
+        </FooterLayout>
      );
 }
  
