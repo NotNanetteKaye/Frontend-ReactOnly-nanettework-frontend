ff3ca7b1bc909af80163d4e21ac3570913788225
NotNanetteKaye <nanettekayedolera@gmail.com>
2023-02-14 23:56:26 -0600

    Laid the foundation of Footer.js

---
+++ nanettework/src/components/NavBar/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return ( 
+        <div>
+            <h4>© 2023 nanette kaye dolera. all rights reserved. </h4>
+        </div>
+     );
+}
+ 
+export default Footer
