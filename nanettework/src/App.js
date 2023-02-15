d53c14bd7b470a767731fc1087719c16f7aec164
NotNanetteKaye <nanettekayedolera@gmail.com>
2023-02-15 00:14:52 -0600

    Began .headerWithTitle section in App.js & App.css

--- nanettework/src/App.js
+++ nanettework/src/App.js
@@ -9,6 +9,9 @@ function App() {
   return (
     <div className='NKDLayout'>
       <NavBar />
+      <div className='headerWithTitle'>
+        <h1>Nanette Kaye Dolera</h1>
+      </div>
       <Footer />
     </div>
   );
