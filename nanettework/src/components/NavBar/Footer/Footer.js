70bcaddcf5a02d51a6e0b81adafd1f11f6712a59
NotNanetteKaye <nanettekayedolera@gmail.com>
2023-02-15 00:04:30 -0600

    Positioning for Footer.js complete!

--- nanettework/src/components/NavBar/Footer/Footer.js
+++ nanettework/src/components/NavBar/Footer/Footer.js
@@ -2,17 +2,22 @@ import React from 'react';
 import styled from 'styled-components';
 
 const FooterLayout = styled.div`
-    background: rgb(231 231 231);
     position: fixed;
+    bottom: 0;
+    left: 0;
+    padding: 1rem;
+    text-align: center;
     width: 100%;
-    display: grid;
-
+    color: black;
+    font: bold 1em Georgia, Times, "Times New Roman", serif;
+    background: pink;
 `
 
 const Footer = () => {
     return ( 
         <FooterLayout>
-            <h4>© 2023 nanette kaye dolera. all rights reserved. </h4>
+            <h4>© 2023 Nanette Kaye Dolera. All rights reserved. </h4>
+            <h6>i will probably include social media logos down here like Cara Cara Wireframe</h6>
         </FooterLayout>
      );
 }
