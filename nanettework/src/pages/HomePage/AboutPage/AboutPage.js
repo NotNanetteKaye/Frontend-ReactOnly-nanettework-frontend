a91b0dd36f47b5c0771e2e9b2a05a89c26522ce4
NotNanetteKaye <nanettekayedolera@gmail.com>
2023-03-01 12:32:41 -0600

    Created AboutPage folder

---
+++ nanettework/src/pages/HomePage/AboutPage/AboutPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+<div className='aboutLayout'>
+<NavBar/>
+<div className='headerWithTitleAbout'>
+</div>
+<div className='bio'>
+</div>
+<div className='footerPic'>
+</div>
+</div>
