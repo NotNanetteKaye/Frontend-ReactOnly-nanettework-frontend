d151516cfc8ce01b010335fdcf601906fa9c6df2
NotNanetteKaye <nanettekayedolera@gmail.com>
2023-03-01 12:43:24 -0600

    Updated aboutWords in HomePage.css

--- nanettework/src/pages/HomePage/HomePage.js
+++ nanettework/src/pages/HomePage/HomePage.js
@@ -13,10 +13,10 @@ const HomePage = () => {
             <div className='aboutImage'>
             </div>
             <div className='aboutWords'>
-              <h1>Mabuhay, I am Nanette</h1>
-              <h5>My name is Nanette (she/her/siya), and I am happy you are here! Raised by a healthcare hero mama and sailor papa, my values remain firm in pursuing a lifestyle that advocates for others while forging my own unforgettable adventures.</h5>
-              <h5>From a young age, I deeply admired the arts. I see the beauty in everything around me, and I hope to encourage others to also bask in life's delights.</h5>
-              <h5>With experiences in coding, photography, and videography, allow me to help you communicate your vision to others by breathing life into your ideas & stories through digital pixels.</h5>
+              <h1>Mabuhay, I am Nanette!</h1>
+              <h5>My name is Nanette (she/her/siya), and I am happy you are here. Raised by a healthcare hero mama and sailor papa, my values remain firm in pursuing a lifestyle that advocates for others while forging my own unforgettable adventures.</h5>
+              <h5>From a young age, I deeply admired the arts. I see beauty in everything around me and want to encourage others to bask in life's delights.</h5>
+              <h5>Experiences in coding, photography, and videography allow me to help others transform their visions, ideas, & stories into reality through the utilization of digital pixels.</h5>
               <button>Resume</button>
             </div>
           </div>
