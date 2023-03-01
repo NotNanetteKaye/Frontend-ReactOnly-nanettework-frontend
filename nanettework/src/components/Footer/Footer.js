84d486f8c0dbb3b56e93f10370f765299867f97b
NotNanetteKaye <nanettekayedolera@gmail.com>
2023-03-01 12:48:14 -0600

    Updated Footer styling in Footer.js

--- nanettework/src/components/Footer/Footer.js
+++ nanettework/src/components/Footer/Footer.js
@@ -2,7 +2,6 @@ import React from 'react';
 import styled from 'styled-components';
 
 const FooterLayout = styled.div`
-    position: fixed;
     bottom: 0;
     left: 0;
     padding: 1rem;
@@ -17,7 +16,6 @@ const Footer = () => {
     return ( 
         <FooterLayout>
             <h4>© 2023 Nanette Kaye Dolera. All rights reserved. </h4>
-            <h6>i will probably include social media logos down here like Cara Cara Wireframe</h6>
         </FooterLayout>
      );
 }
