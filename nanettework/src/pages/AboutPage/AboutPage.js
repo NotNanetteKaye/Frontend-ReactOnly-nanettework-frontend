b43c971e39b96ae99df70fe70bbbdcde7cfed6e7
NotNanetteKaye <nanettekayedolera@gmail.com>
2023-03-01 12:57:24 -0600

    Created AboutPage.css in pages folder

--- nanettework/src/pages/AboutPage/AboutPage.js
+++ nanettework/src/pages/AboutPage/AboutPage.js
@@ -1,11 +1,27 @@
+// General Imports
 import React from "react";
+import '../AboutPage/AboutPage.css';
 
-<div className='aboutLayout'>
-<NavBar/>
-<div className='headerWithTitleAbout'>
-</div>
-<div className='bio'>
-</div>
-<div className='footerPic'>
-</div>
-</div>
+
+
+const AboutPage = () => {
+    return (
+        <div className='NKDLayout'>
+          <div className='headerWithTitle'>
+          </div>
+          <div className='about'>
+            <div className='aboutImage'>
+            </div>
+            <div className='aboutWords'>
+              <h1>Mabuhay, I am Nanette!</h1>
+              <h5>My name is Nanette (she/her/siya), and I am happy you are here. Raised by a healthcare hero mama and sailor papa, my values remain firm in pursuing a lifestyle that advocates for others while forging my own unforgettable adventures.</h5>
+              <h5>From a young age, I deeply admired the arts. I see beauty in everything around me and want to encourage others to bask in life's delights.</h5>
+              <h5>Experiences in coding, photography, and videography allow me to help others transform their visions, ideas, & stories into reality through the utilization of digital pixels.</h5>
+              <button>Resume</button>
+            </div>
+          </div>
+        </div>
+      );
+}
+ 
+export default AboutPage;
