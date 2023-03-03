786aae229a58ef2026476dd87d1778d70aa44ab4
NotNanetteKaye <nanettekayedolera@gmail.com>
2023-03-02 21:30:29 -0600

    I href my resume in HomePage.js

--- nanettework/src/pages/HomePage/HomePage.js
+++ nanettework/src/pages/HomePage/HomePage.js
@@ -17,7 +17,8 @@ const HomePage = () => {
               <h5>My name is Nanette (she/her/siya), and I am happy you are here. Raised by a healthcare hero mama and sailor papa, my values remain firm in pursuing a lifestyle that advocates for others while forging my own unforgettable adventures.</h5>
               <h5>From a young age, I deeply admired the arts. I see beauty in everything around me and want to encourage others to bask in life's delights.</h5>
               <h5>Experiences in coding, photography, and videography allow me to help others transform their visions, ideas, & stories into reality through the utilization of digital pixels.</h5>
-              <button>Resume</button>
+              <a href='https://drive.google.com/file/d/10Ghf2zFESwT6ql9p6OKEas-PA50w8n3d/view'>Resume</a>
+
             </div>
           </div>
         </div>
