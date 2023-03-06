5b8e197cb85d2fab4bcf1d8363fbee6ddb53b929
NotNanetteKaye <nanettekayedolera@gmail.com>
2023-03-05 18:45:49 -0600

    ¡El estilo del botón de reanudar se ve bien!

--- nanettework/src/pages/HomePage/HomePage.js
+++ nanettework/src/pages/HomePage/HomePage.js
@@ -1,6 +1,6 @@
 // General imports
 import React from 'react';
-import '../HomePage/HomePage.css'
+import '../HomePage/HomePage.css';
 
 // Component imports
 
@@ -17,11 +17,14 @@ const HomePage = () => {
               <h5>My name is Nanette (she/her/siya), and I am happy you are here. Raised by a healthcare hero mama and sailor papa, my values remain firm in pursuing a lifestyle that advocates for others while forging my own unforgettable adventures.</h5>
               <h5>From a young age, I deeply admired the arts. I see beauty in everything around me and want to encourage others to bask in life's delights.</h5>
               <h5>Experiences in coding, photography, and videography allow me to help others transform their visions, ideas, & stories into reality through the utilization of digital pixels.</h5>
-              <a href='https://drive.google.com/file/d/10Ghf2zFESwT6ql9p6OKEas-PA50w8n3d/view'
+              <div style={{marginTop: '5mm', backgroundImage: 'linear-gradient(#aaa642, #807e1a)', width: '54px', borderRadius:'3px', borderStyle:'solid', borderColor:'#b2aba9', padding: '1mm'}}>
+                <a href='https://drive.google.com/file/d/10Ghf2zFESwT6ql9p6OKEas-PA50w8n3d/view'
               target="_blank"
-              rel="noreferrer">
-               Resume
-              </a>
+              rel="noreferrer"
+              style={{color: '#e7e7e7'}}>
+               resume!
+                </a>
+              </div>
             </div>
           </div>
         </div>
