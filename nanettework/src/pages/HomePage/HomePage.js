bf3a3533bc8177511a7c538889c4c6ff305511b7
NotNanetteKaye <nanettekayedolera@gmail.com>
2023-03-05 19:09:00 -0600

    El sombreado del botón se ve hermosa

--- nanettework/src/pages/HomePage/HomePage.js
+++ nanettework/src/pages/HomePage/HomePage.js
@@ -17,11 +17,11 @@ const HomePage = () => {
               <h5>My name is Nanette (she/her/siya), and I am happy you are here. Raised by a healthcare hero mama and sailor papa, my values remain firm in pursuing a lifestyle that advocates for others while forging my own unforgettable adventures.</h5>
               <h5>From a young age, I deeply admired the arts. I see beauty in everything around me and want to encourage others to bask in life's delights.</h5>
               <h5>Experiences in coding, photography, and videography allow me to help others transform their visions, ideas, & stories into reality through the utilization of digital pixels.</h5>
-              <div style={{marginTop: '5mm', backgroundImage: 'linear-gradient(#aaa642, #807e1a)', width: '54px', borderRadius:'3px', borderStyle:'solid', borderColor:'#b2aba9', padding: '1mm'}}>
+              <div className='resumeButton'>
                 <a href='https://drive.google.com/file/d/10Ghf2zFESwT6ql9p6OKEas-PA50w8n3d/view'
               target="_blank"
               rel="noreferrer"
-              style={{color: '#e7e7e7'}}>
+              style={{borderRadius: '2mm'}}>
                resume!
                 </a>
               </div>
