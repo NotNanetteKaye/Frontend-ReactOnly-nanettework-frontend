caafa95f58714889c6328b354617c8a6e32be89c
NotNanetteKaye <nanettekayedolera@gmail.com>
2023-03-07 12:40:46 -0600

    new images + imported React carousel in PortfPage

--- nanettework/src/pages/PortfolioPage/PortfolioPage.js
+++ nanettework/src/pages/PortfolioPage/PortfolioPage.js
@@ -1,11 +1,45 @@
 import React from "react";
+import Carousel from 'react-multi-carousel';
+import { FaReact } from 'react-icons/fa'
+import 'react-multi-carousel/lib/styles.css'
 import '../PortfolioPage/PortfolioPage.css'
 
+
+
 const PortfolioPage = () => {
+    const responsive = {
+        desktop: {
+        superLargeDesktop: {
+            // the naming can be any, depends on you.
+            breakpoint: { max: 4000, min: 3000 },
+            items: 8
+        },
+          breakpoint: { max: 3000, min: 1024 },
+          items: 3,
+          slidesToSlide: 3 // optional, default to 1.
+        },
+        tablet: {
+          breakpoint: { max: 1024, min: 464 },
+          items: 2,
+          slidesToSlide: 2 // optional, default to 1.
+        },
+        mobile: {
+          breakpoint: { max: 464, min: 0 },
+          items: 1,
+          slidesToSlide: 1 // optional, default to 1.
+        }
+      };
+
     return(
         <div className='portfolioPageLayout'>
             <div className='portfolioHeaderTitle'>
                 <h1>PORTFOLIO</h1>
+                <div className='carouselSample'>
+                    <CarouselComponent>
+
+                    </CarouselComponent>
+                </div>
+                <FaReact size={100} />
             </div>
             <div className='codingScene'>
                 <h2>CODING PROJECTS</h2>
