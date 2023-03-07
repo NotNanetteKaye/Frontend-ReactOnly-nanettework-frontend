21d69f81d1c55ad1e0eefeebb110cf03d61c16ed
NotNanetteKaye <nanettekayedolera@gmail.com>
2023-03-07 13:51:35 -0600

    <Carousel></> in PortPage.js works!

--- nanettework/src/pages/PortfolioPage/PortfolioPage.js
+++ nanettework/src/pages/PortfolioPage/PortfolioPage.js
@@ -1,65 +1,32 @@
-import React from "react";
-import Carousel from 'react-multi-carousel';
+import React, { Component } from 'react';
+import Carousel from 'react-elastic-carousel';
 import { FaReact, FaPython, FaHtml5, FaCss3Alt } from 'react-icons/fa';
 import { SiDjango, SiJavascript } from 'react-icons/si';
 import 'react-multi-carousel/lib/styles.css'
 import '../PortfolioPage/PortfolioPage.css'
 
 
-
 const PortfolioPage = () => {
-    const responsive = {
-        desktop: {
-        superLargeDesktop: {
-            // the naming can be any, depends on you.
-            breakpoint: { max: 4000, min: 3000 },
-            items: 8
-        },
-          breakpoint: { max: 3000, min: 1024 },
-          items: 3,
-          slidesToSlide: 3 // optional, default to 1.
-        },
-        tablet: {
-          breakpoint: { max: 1024, min: 464 },
-          items: 2,
-          slidesToSlide: 2 // optional, default to 1.
-        },
-        mobile: {
-          breakpoint: { max: 464, min: 0 },
-          items: 1,
-          slidesToSlide: 1 // optional, default to 1.
-        }
-      };
+    const carouselIcons = [
+        { width: 1, itemsToShow: 1},
+        { width: 1, itemsToShow: 2},
+        { width: 1, itemsToShow: 3},
+      ]
+    
 
     return(
         <div className='portfolioPageLayout'>
             <div className='portfolioHeaderTitle'>
                 <h1>PORTFOLIO</h1>
-                <div className="w-full relative h-32">
-                    <Carousel
-                        infinite={true}
-                        responsive={responsive}
+                <div className='Carousel'>
+                    <Carousel breakPoints={carouselIcons}
                     >
-                        <div style={{display:'flex', flex:'row', justifyContent:'center'}}>
-                            <div className='carouselComponents'>
-                                <FaReact size={100} />
-                            </div>
-                            <div className='carouselComponents'>
-                                <FaPython size={100} />
-                            </div>
-                            <div className='carouselComponents'>
-                                <FaHtml5 size={100}/>
-                            </div>
-                            <div className='carouselComponents'>
-                                <FaCss3Alt size={100} />
-                            </div>
-                            <div className='carouselComponents'>
-                                <SiDjango size={100} />
-                            </div>
-                            <div className='carouselComponents'>
-                                <SiJavascript size={100} />
-                            </div>
-                        </div>
+                        <FaReact size={100} />
+                        <FaPython size={100} />
+                        <FaHtml5 size={100}/>
+                        <FaCss3Alt size={100} />
+                        <SiDjango size={100} />
+                        <SiJavascript size={100} />
                     </Carousel>
                 </div>
             </div>
