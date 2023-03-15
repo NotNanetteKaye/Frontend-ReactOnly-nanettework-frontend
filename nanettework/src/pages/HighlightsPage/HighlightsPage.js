1657281d02fce27dea7f7e67195fdacc50aea1f7
NotNanetteKaye <nanettekayedolera@gmail.com>
2023-03-14 21:46:33 -0500

    Edited Highlights h3 in HighlightsPage.js

--- nanettework/src/pages/HighlightsPage/HighlightsPage.js
+++ nanettework/src/pages/HighlightsPage/HighlightsPage.js
@@ -7,7 +7,12 @@ import './HighlightsPage.css';
 const HighlightsPage = () => {
     return (
         <div className='aboutLayout'>
-          <div className='headerWithTitleAbout'></div>
+          <div className='headerWithTitleAbout'>
+            <div></div>
+            <div>
+              <h3 style={{display: 'flex', flexDirection: 'column', flexWrap: 'wrap', alignContent: 'space-around', marginLeft: '20mm', textShadow: '#0000003d 1px 0 10px', color: '#e7e7e7'}}>HIGHLIGHTS</h3>
+            </div>
+          </div>
           <div className='bio'>
           </div>
           <div className='footerPic'>
