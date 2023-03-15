5879f2a1d7d95146fff7e08266613b0942bf4f84
NotNanetteKaye <nanettekayedolera@gmail.com>
2023-03-14 22:03:19 -0500

    Added 2021 highlights to HighlightsPg.js

--- nanettework/src/pages/HighlightsPage/HighlightsPage.js
+++ nanettework/src/pages/HighlightsPage/HighlightsPage.js
@@ -18,6 +18,9 @@ const HighlightsPage = () => {
             <h3>August 6 2022: a day very special to me because I officially became the first person in my family to complete an American university! We were decked out or whatever during ceremony. ✨</h3>
             <h3>I recieved an award from the Texas Tech University recognizing my campus-wide efforts in advancing diversity, equity, and inclusion. Wreck 'em for life!</h3>
             <h3>I spent my senior year dedicating my thesis to the relationship between the immigration status and a child's well being. Publication details OTW!</h3>
+            <h2>2021</h2>
+            <h3>I had my first Oprah moment during the annual First Generation Week where I got to interview the TTU Honors College Dean!</h3>
+            <h3>I co-lead LatinX/Hispanic Heritage Month alongside one of the best humans I met during my time at Texas Tech University. Check us out here :)) </h3>
           </div>
         </div>
       );
