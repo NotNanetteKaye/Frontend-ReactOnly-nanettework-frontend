de753122e5faa0ba6ef78b59b62749a5f571cd0f
NotNanetteKaye <nanettekayedolera@gmail.com>
2023-03-14 22:00:18 -0500

    Added 2022 highlights in HighlightsPage.js

--- nanettework/src/pages/HighlightsPage/HighlightsPage.js
+++ nanettework/src/pages/HighlightsPage/HighlightsPage.js
@@ -14,6 +14,10 @@ const HighlightsPage = () => {
             </div>
           </div>
           <div className='bio'>
+            <h2>2022</h2>
+            <h3>August 6 2022: a day very special to me because I officially became the first person in my family to complete an American university! We were decked out or whatever during ceremony. ✨</h3>
+            <h3>I recieved an award from the Texas Tech University recognizing my campus-wide efforts in advancing diversity, equity, and inclusion. Wreck 'em for life!</h3>
+            <h3>I spent my senior year dedicating my thesis to the relationship between the immigration status and a child's well being. Publication details OTW!</h3>
           </div>
         </div>
       );
