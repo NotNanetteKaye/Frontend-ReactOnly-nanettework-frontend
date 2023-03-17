6f4e092e143f102263ccfb090104b96130d229f9
NotNanetteKaye <nanettekayedolera@gmail.com>
2023-03-17 16:07:33 -0500

    Simplified code in HLPage.js & .css

--- nanettework/src/pages/HighlightsPage/HighlightsPage.js
+++ nanettework/src/pages/HighlightsPage/HighlightsPage.js
@@ -5,15 +5,13 @@ import Footer from '../../components/Footer/Footer'
 
 
 // 🤔: add motion to 'HEADINGS'
-// 🤔: fix format of the h2 & h3
 // 🤔: hyperlink elements of H3 to respective articles
 const HighlightsPage = () => {
     return (
         <div className='aboutLayout'>
           <div className='headerWithTitleAbout'>
-            <div></div> 
-            <div>
-              <h3 style={{display: 'flex', flexDirection: 'column', flexWrap: 'wrap', alignContent: 'space-around', marginLeft: '20mm', textShadow: '#0000003d 1px 0 10px', color: '#e7e7e7'}}>HIGHLIGHTS</h3>
+            <div className='headerTitle'>
+              <h3 style={{textShadow: '#0000003d 1px 0 10px', color: '#e7e7e7'}}>HIGHLIGHTS</h3>
             </div>
           </div>
           <div className='bio'>
