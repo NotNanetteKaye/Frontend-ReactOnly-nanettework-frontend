59a4fc53f8ac98a77ba3da6b756ec4a857b51f4a
NotNanetteKaye <nanettekayedolera@gmail.com>
2023-03-19 22:35:54 -0500

    properly link resume in HomePage.js

--- nanettework/src/pages/HomePage/HomePage.js
+++ nanettework/src/pages/HomePage/HomePage.js
@@ -25,7 +25,7 @@ const HomePage = () => {
               <h5>From a young age, I deeply admired the arts. I see beauty in everything around me and want to encourage others to bask in life's delights.</h5>
               <h5>Experiences in coding, photography, and videography allow me to help others transform their visions, ideas, & stories into reality through the utilization of digital pixels.</h5>
               <div className='resumeButton'>
-                <a href=''
+                <a href='https://docs.google.com/document/d/1pGIYk2sPJc_T1TTKB1xkXV5Fi4P3vjlUrOiKNnw4NMQ/edit?usp=sharing'
               target="_blank"
               rel="noreferrer">
                resume!
