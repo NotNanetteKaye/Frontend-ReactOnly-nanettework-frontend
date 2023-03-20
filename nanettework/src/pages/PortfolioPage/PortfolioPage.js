499bd03c81f65945a3353552d8dd8ce41a56f0da
NotNanetteKaye <nanettekayedolera@gmail.com>
2023-03-19 22:01:09 -0500

    CODING PROJECT HOVER WORKSSSS

--- nanettework/src/pages/PortfolioPage/PortfolioPage.js
+++ nanettework/src/pages/PortfolioPage/PortfolioPage.js
@@ -5,9 +5,9 @@ import recruitmentVideo from '../../portfolio_images/horizontalVideo.png';
 import revealVideo from '../../portfolio_images/verticalVideo.png';
 
 // CSS Imports
-import 'react-multi-carousel/lib/styles.css';
 import '../PortfolioPage/PortfolioPage.css';
 
+// React Imports
 import React from 'react';
 import { FaReact, FaPython, FaHtml5, FaCss3Alt } from 'react-icons/fa';
 import { SiDjango, SiJavascript, SiMysql } from 'react-icons/si';
@@ -17,7 +17,7 @@ import Footer from '../../components/Footer/Footer';
 
 const projectVariant = {
     hidden: { opacity: 0, scale: 0.8 },
-    visible: { opacity: 1, scale: 1, transition: {type: 'spring'}},
+    visible: { opacity: 1, scale: 1, transition: {type: 'spring', delay: .6}},
 };
 
 const Project = ({ title, description, link, photo }) => {
@@ -30,13 +30,13 @@ const Project = ({ title, description, link, photo }) => {
             target="_blank"
             rel="noreferrer"
         >
-                <div className='individualProjects'>
-                    <img src={photo} alt={title} width='%' height='50%'/>
-                    <div className='individualProjectDetails'>
-                        <h3>{title}</h3>
-                         <p>{description}</p>
-                    </div>
+            <div className='individualProjectContainer'>
+                <img src={photo} alt={title} width='%' height='50%'/>
+                <div className='individualProjectWords'>
+                    <h4>{title}</h4>
+                    <p>{description}</p>
                 </div>
+            </div>
         </a>
         </motion.div>
     );
@@ -83,14 +83,14 @@ const PortfolioPage = () => {
                 <h1>CODING PROJECTS</h1>
                 <div className='projectDisplay'>
                     <motion.div>
-                        <Project title='Darling Dreams'
-                        description='Find black/brown artists & entrepreneurs in Texas'
+                        <Project title='Darling Dreams (2023 Capstone)'
+                        description='A full-stack application to find & support your black/brown artists & entrepreneurs around Texas/the United States!'
                         link='https://github.com/NotNanetteKaye/capstone2022'
                         photo= {darlingDreams} />
                     </motion.div>
                     <motion.div>
-                        <Project title='Youtube Minus the Ads'
-                            description='A video player full-stack application utilizing the YoutubeAPI with user ability to leave comments'
+                        <Project title='Youtube Minus the Ads (2022)'
+                            description='A full-stack application utilizing the YoutubeAPI with user ability to add, delete, and view comments.'
                             link='https://github.com/NotNanetteKaye/youtube-clone'
                             photo = {youtubeClone} />
                     </motion.div>
@@ -100,14 +100,14 @@ const PortfolioPage = () => {
                 <h1>VIDEOGRAPHY</h1>
                 <div className='projectDisplay'>
                     <motion.div>
-                        <Project title='Reveal Video'
-                            description='Full House inspired class reveal video'
+                        <Project title='Delta Phi Omega 2020 Fall Class Reveal Video'
+                            description='I created a Full House inspired class reveal video to virtually introduce our Fall 2020 Recruits to the Multicultural Greek Council at Texas Tech University. '
                             link='https://www.instagram.com/reel/CH_MO4tni4A/'
                             photo= {revealVideo} />
                     </motion.div>
                     <motion.div>
-                        <Project title='DPO 2020 Recruitment Video'
-                            description='Video inspired by the exarcebated digital dependance due to COVID-19'
+                        <Project title='Delta Phi Omega 2020 Fall Recruitment Video'
+                            description='I created a technology-inspired video for fall recruitment, which was inspired by the exarcebated digital dependance caused by the COVID-19 pandemic.'
                             link='https://www.instagram.com/p/CE7_xAABfiu/'
                             photo= {recruitmentVideo} />
                     </motion.div>
