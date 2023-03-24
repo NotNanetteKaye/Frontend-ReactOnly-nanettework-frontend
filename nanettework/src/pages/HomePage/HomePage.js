2ef33cb40f295e4579464fdfde09ba54314ed9db
NotNanetteKaye <nanettekayedolera@gmail.com>
2023-03-24 13:34:32 -0500

    Altered wording in HomePage.js & .css

--- nanettework/src/pages/HomePage/HomePage.js
+++ nanettework/src/pages/HomePage/HomePage.js
@@ -7,24 +7,24 @@ import nani from '../../portfolio_images/nanifrontpic.jpg'
 import Footer from '../../components/Footer/Footer'
 
 // TO-DO'S
-// 🤔: Logo for personal portfolio
+// 🤔: Replace logo for personal portfolio
 const HomePage = () => {
     return (
         <div className='NKDLayout'>
           <div className='headerImage'>
             <img src={nani} alt='Nani' width={'100%'}/>
           </div>
-          
-          <div className='about'>
-            <div id='image'></div>
-            <div id='words'>
-              <div className='aboutWords'>
+
+          <div className='aboutContainer'>
+            <div className='imageContainer'></div>
+            <div className='wordsContainer'>
+              <div id='aboutWords'>
               <h1>Mabuhay, I am Nanette!</h1>
               <h5>My name is Nanette (she/her/siya), and I am happy you are here. Raised by a healthcare hero mama and sailor papa, my values remain firm in pursuing a lifestyle that advocates for others while forging my own unforgettable adventures.</h5>
               <h5>From a young age, I deeply admired the arts. I see beauty in everything around me and want to encourage others to bask in life's delights.</h5>
               <h5>Experiences in coding, photography, and videography allow me to help others transform their visions, ideas, & stories into reality through the utilization of digital pixels.</h5>
-              <div className='resumeButton'>
-                <a href='https://docs.google.com/document/d/1pGIYk2sPJc_T1TTKB1xkXV5Fi4P3vjlUrOiKNnw4NMQ/edit?usp=sharing'
+              <div id='resumeButton'>
+                <a href='https://docs.google.com/document/d/1pS1E-Np_G6mai3XbuuLbHcpu6ID4KrJAI7fOxEQSPXk/edit?usp=sharing'
               target="_blank"
               rel="noreferrer">
                resume!
@@ -33,8 +33,6 @@ const HomePage = () => {
               </div>
             </div>
           </div>
-
-
           <Footer/>
         </div>
       );
